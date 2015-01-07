063ba8acd4c31f1b0b5ebd3ff9315860f8056aa3
Tanguy Pedrazzoli <titicoolrasta@gmail.com>
2015-01-07 14:49:21 +0100

    Push prism

---
+++ HMP_PRISM/HMP.Desktop.Core/ViewModels/LeftSideViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+
+namespace HMP.Desktop.Core.ViewModels
+{
+    [Export]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class LeftSideViewModel
+    {
+        public LeftSideViewModel() { }
+    }
+}
---
+++ HMP_PRISM/HMP.Desktop.Module/RootModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+
+namespace HMP.Desktop.Module
+{
+    public class RootModule : IModule
+    {
+        private readonly IRegionManager regionManager;
+
+        public RootModule(IRegionManager regionManager)
+        {
+            this.regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            regionManager.RegisterViewWithRegion("MainRegion", typeof(Views.ModuleView));
+        }
+    }
+}
