6034e1b38bc6a00c7afc7873f4007fdf211762ed
Tanguy Pedrazzoli <titicoolrasta@gmail.com>
2015-01-07 16:30:03 +0100

    gestion module ok

---
+++ HMP_PRISM/HMP.Desktop.Test/test.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMP.Desktop.Test
+{
+    public class test : IModule
+    {
+        private readonly IRegionManager regionManager;
+
+        public test(IRegionManager regionManager)
+        {
+            this.regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            regionManager.RegisterViewWithRegion("SubRegion", typeof(Views.view));
+        }
+    }
+}
