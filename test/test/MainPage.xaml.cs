e92445801609e04ac0dc30ff2d233c9152c59a70
kunt22 <guillaumejparis@gmail.com>
2015-01-07 14:59:15 +0100

    page principale

---
+++ test/test/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
+
+// Pour en savoir plus sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace test
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            TileCanvas tc = new TileCanvas();
+            BitmapImage img = new BitmapImage();
+            Uri baseUri = new Uri("ms-appx:///");
+            img.UriSource = new Uri(baseUri, "Assets/FilmStrip.png");
+            tc.ImageSource = img;
+            FilmStrip.Children.Add(tc);
+        }
+    }
+}
